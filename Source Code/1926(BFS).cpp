ee11d531dfdead046353a4be49f32230ea5b6407
Raccooon98 <redbull1171@gmail.com>
2024-02-28 18:30:01 +0900

    24_02_27.md추가

--- Source Code/1926(BFS).cpp
+++ Source Code/1926(BFS).cpp
@@ -12,24 +12,24 @@ int dy[] = { 0,1,0,-1 };  //탐색 방향 오 위 왼 아래
 queue<pair<int, int>> q;//BFS 큐
 vector<int> count_pic; //그림개수 벡터
 int w = 1;
-int nx, ny;
+
 void BFS(int x, int y) {
-	vis[y][x] = 1; //방문 표시
-	q.push(make_pair(y, x));
+	vis[x][y] = 1; //방문 표시
+	q.push(make_pair(x, y));
 	while (!q.empty()) {
 		x = q.front().first;
 		y = q.front().second;
 		q.pop();
 		for (int i = 0; i < 4; i++) {
-			nx = x + dx[i];
-			ny = y + dy[i];
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 			if (ny < 0 || nx < 0 || nx >= n || ny >= m) {
 				continue;
-				if (board[ny][nx] == 1 && vis[ny][nx] == 0) {
-					vis[ny][nx] = 1;
-					w++;
-					q.push(make_pair(y, x));
 			}
+			if (board[nx][ny] == 1 && vis[nx][ny] == 0) {
+				vis[nx][ny] = 1;
+				w++;
+				q.push(make_pair(nx, ny));
 			}
 		}
 
@@ -41,7 +41,6 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++)
 			cin >> board[i][j];
-
 	}
 
 	int cnt = 0;
@@ -56,9 +55,12 @@ int main() {
 		}
 	}
 
-	int maxw = *max_element(count_pic.begin(), count_pic.end());
+	//int maxw = *max_element(count_pic.begin(), count_pic.end());
+	sort(count_pic.begin(), count_pic.end());
 	cout << cnt << '\n';
-	cout << maxw << '\n';
-
+	if (count_pic.empty())
+		cout << 0 << '\n';
+	else
+		cout << *max_element(count_pic.begin(), count_pic.end()) << '\n';
 
 }
