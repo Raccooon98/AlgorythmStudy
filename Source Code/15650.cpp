62d06e4d6ad0ea14b5b213a93e0b795515d1dddd
Raccooon98 <redbull1171@gmail.com>
2024-03-06 21:13:38 +0900

    update 03_06

---
+++ Source Code/15650.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<algorithm> // sort 함수를 사용하기 위해 추가
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<vector<int>> result;
+bool vis[10];
+
+void DFS(int n) {
+	if (n == M) {
+		result.push_back(arr); // 현재 조합을 결과 벡터에 추가
+		return;
+	}
+	else {
+		for (int i = 1; i <= N; i++) {
+			if (!vis[i]) {
+				arr.push_back(i);
+				vis[i] = 1;
+				DFS(n + 1);
+				arr.pop_back();
+				vis[i] = 0;
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> N >> M;
+	DFS(0);
+
+	// 각 조합을 오름차순으로 정렬
+	for (auto &comb : result) {
+		sort(comb.begin(), comb.end());
+	}
+
+	// 중복 제거
+	sort(result.begin(), result.end());
+	result.erase(unique(result.begin(), result.end()), result.end());
+
+	// 결과 출력
+	for (auto &comb : result) {
+		for (int num : comb) {
+			cout << num << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
