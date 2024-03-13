68dbc976ffa3249ed9a4b5381ada7393fcce946c
Raccooon98 <redbull1171@gmail.com>
2024-03-13 19:40:04 +0900

    update_1

--- Source Code/5427.cpp
+++ Source Code/5427.cpp
@@ -1,17 +1,17 @@
-//불!문제랑 비슷한 방식으로 구현 시도 BFS
-
 #include<iostream>
 #include<queue>
 #include<vector>
+#include<cstring>
 using namespace std;
-using pii = pair<int,int>;
-int w, h, T,cnt=0;
+using pii = pair<int, int>;
+int w, h, T, cnt = 0;
+bool escape = false;
 char map[1001][1001];//원래string 으로 받으려 했는데 @ * 판단해서 불, 상근 큐에 추가하기 위해 char로 받음
-int fvis[1001][1001] = {0,};
-int svis[1001][1001] = {0,};
+int fvis[1001][1001] = { 0, };
+int svis[1001][1001] = { 0, };
 // 드디어 상하좌우
-int dx[] = {1,0,-1,0};
-int dy[] = {0,1,0,-1};
+int dx[] = { 1,0,-1,0 };
+int dy[] = { 0,1,0,-1 };
 queue<pii> fq;
 queue<pii> sq;
 
@@ -23,7 +23,7 @@ void BFS() {
 		for (auto i = 0; i < 4; ++i) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
-			if (nx < 0 || ny < 0 || nx >= w || ny >= h) continue;
+			if (nx < 0 || ny < 0 || ny >= w || nx >= h) continue;
 			if (map[nx][ny] == '#' || fvis[nx][ny] != 0) continue;
 			fvis[nx][ny] = fvis[x][y] + 1;
 			fq.push({ nx,ny });
@@ -37,19 +37,27 @@ void BFS() {
 		for (auto i = 0; i < 4; ++i) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
-			if (nx < 0 || ny < 0 || nx >= w || ny >= h) continue;
+			if (nx < 0 || ny < 0 || ny >= w || nx >= h) {
+				cout << svis[x][y] << '\n';
+				escape = true;
+				break;
+			}
 			if (map[nx][ny] == '#' || map[nx][ny] == '*') continue;
 			if (svis[nx][ny] != 0) continue;
-			if ((svis[x][y] + 1 >= fvis[nx][ny])) continue;
+			if ((svis[x][y] + 1 >= fvis[nx][ny])&&fvis[nx][ny]!=0) continue;
 			svis[nx][ny] = svis[x][y] + 1;
 			sq.push({ nx,ny });
 		}
+		if (escape)
+			break;
 	}
+
 }
 
 int main(void) {
 	cin >> T;
 	while (T--) {//테스트 케이스만큼 돌리기 for문말고 while루프
+		escape = false;
 		cin >> w >> h;
 		for (auto i = 0; i < h; ++i) {
 			for (auto j = 0; j < w; ++j) {
@@ -58,19 +66,23 @@ int main(void) {
 					sq.push({ i,j });
 					svis[i][j] = 1;
 				}
-					
 				else if (map[i][j] == '*') {
 					fq.push({ i,j });
 					fvis[i][j] = 1;
 				}
+				else {
+					svis[i][j] = 0;
+					fvis[i][j] = 0;
+				}
+				
 			}
 		}
 		BFS();
-		if (cnt <= 0)
-			cout << "IMPOSSIBLE" << '\n';
-		cout << cnt<<'\n';
+		if (!escape)
+			cout << "IMPOSSIBLE" << "\n";
 		//초기화
 		memset(map, '.', sizeof(map));
+
 		while (!fq.empty())
 			fq.pop();
 		while (!sq.empty())
